dfb655ca9f201e1f3ac671cc827b08a2d4c2ddf8
dhuggins221 <dhuggins221@gmail.com>
2021-08-23 22:58:41 -0500

    1.0

---
+++ routes/api/thoughts-routes.js
@@ -0,0 +1,20 @@
+const router = require("express").Router();
+const {
+  getAllThought,
+  getThoughtById,
+  createThought,
+  updateThought,
+  deleteThought,
+  addReaction,
+  removeReaction,
+} = require("../../controllers/thought-controller");
+
+router.route("/").get(getAllThought).post(createThought);
+
+router
+  .route("/:id")
+  .get(getThoughtById)
+  .put(updateThought)
+  .delete(deleteThought);
+
+module.exports = router;
